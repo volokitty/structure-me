e6bf5222d8c85ad3f84c5e3f018bdd720e995025
volokitty <vlad.volokitin.91@gmail.com>
2020-06-25 22:36:28 +0300

    Setting up the settings.json file has become clearer. At the same time, the parser has been changed.

--- scripts/main.py
+++ scripts/main.py
@@ -6,13 +6,15 @@ path = sys.argv[1].replace('\\', '\\\\') + '\\\\'
 
 def parse(dictionary, path='D:\\Code\\test\\'):  
     for key in dictionary:
-        if dictionary[key] == 0: # empty folder
+        if dictionary[key] == "emptyFolder": # empty folder
             os.mkdir(path + key + r'\\')
-        if dictionary[key] == 1: # file
+        if dictionary[key] == "emptyFile": # file
             file = open(path + key, 'w')
             file.close()
         if dictionary[key] == 'jshint': # .jshintrc
             shutil.copy2(r'D:\\Code\\scripts\\2020\\structure-me\\settings\\.jshintrc', path + r'\\.jshintrc')
+        if dictionary[key] == 'style.scss':
+            shutil.copy2(r'D:\\Code\\scripts\\2020\\structure-me\\settings\\style.scss', path + r'\\style.scss')
         if type(dictionary[key]) == type(dictionary): # directory
             os.mkdir(path + key + r'\\')
             parse(dictionary[key], path + key + r'\\')
