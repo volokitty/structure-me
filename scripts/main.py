f27033b22b3ec0c6c088cfeeef4ee10dcd87dd60
volokitty <vlad.volokitin.91@gmail.com>
2020-06-25 23:21:14 +0300

    Changes

--- scripts/main.py
+++ scripts/main.py
@@ -1,23 +1,24 @@
 # Vlad Volokitin (volokitty)
 
-import os, sys, json, shutil
+import os, sys, json, shutil, pathlib
 
-path = sys.argv[1].replace('\\', '\\\\') + '\\\\'
+path = sys.argv[1]
+cwd = pathlib.Path(os.getcwd())
 
 def parse(dictionary, path):  
     for key in dictionary:
         if dictionary[key] == "emptyFolder": # empty folder
-            os.mkdir(path + key + r'\\')
+            os.mkdir(os.path.join(path, key, ''))
         if dictionary[key] == "emptyFile": # file
-            file = open(path + key, 'w')
+            file = open(os.path.join(path, key), 'w')
             file.close()
         if dictionary[key] == 'jshint': # .jshintrc
-            shutil.copy2(r'D:\\Code\\scripts\\2020\\structure-me\\settings\\.jshintrc', path + r'\\.jshintrc')
+            shutil.copy2(os.path.join(cwd.parent.parent, r'settings\.jshintrc'), os.path.join(path, '.jshintrc'))
         if dictionary[key] == 'style.scss':
-            shutil.copy2(r'D:\\Code\\scripts\\2020\\structure-me\\settings\\style.scss', path + r'\\style.scss')
+            shutil.copy2(os.path.join(cwd.parent.parent, r'settings\.jshintrc'), os.path.join(path, 'style.scss'))
         if type(dictionary[key]) == type(dictionary): # directory
-            os.mkdir(path + key + r'\\')
-            parse(dictionary[key], path + key + r'\\')
+            os.mkdir(os.path.join(path, key, ''))
+            parse(dictionary[key], os.path.join(path, key, ''))
 
 def main():
     json_data = open(r'D:\\Code\\scripts\\2020\\structure-me\\settings\\settings.json', 'r')
--- scripts/reg.py
+++ scripts/reg.py
@@ -5,7 +5,6 @@ main_script = sys.executable
 key_path = r'Directory\Background\shell\structureME'
 key = winreg.CreateKeyEx(winreg.HKEY_CLASSES_ROOT, key_path)
 winreg.SetValue(key, '', winreg.REG_SZ, '')
-# winreg.SetValueEx(key, 'Icon', 0, winreg.REG_SZ, main_script + f' "{cwd}\\main.py"')
 
 command_key = winreg.CreateKeyEx(key, 'command')
 winreg.SetValue(command_key, '', winreg.REG_SZ, main_script + f' "{cwd}\\main.py" "%V"')
