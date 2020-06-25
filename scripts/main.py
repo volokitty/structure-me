816bf2559698bc8b0045b22baf7e8467f3700499
volokitty <vlad.volokitin.91@gmail.com>
2020-06-25 17:40:16 +0300

    Deleted some trash lines of code

--- scripts/main.py
+++ scripts/main.py
@@ -4,37 +4,23 @@ import os, sys, json, shutil
 
 path = sys.argv[1].replace('\\', '\\\\') + '\\\\'
 
-path_file = open('D:\\Code\\m\\path.txt', 'w')
-
 def parse(dictionary, path='D:\\Code\\test\\'):  
-    path_file.write('parse\n')
     for key in dictionary:
         if dictionary[key] == 0: # empty folder
-            print('Creating an empty folder')
-            path_file.write('Creating an empty folder\n')
             os.mkdir(path + key + r'\\')
         if dictionary[key] == 1: # file
-            print('Creating a file')
-            path_file.write('Creating a file\n')
             file = open(path + key, 'w')
             file.close()
         if dictionary[key] == 'jshint': # .jshintrc
-            print('Creating a jshint file')
-            path_file.write('Creating a jshint file\n')
             shutil.copy2(r'D:\\Code\\scripts\\2020\\structure-me\\settings\\.jshintrc', path + r'\\.jshintrc')
         if type(dictionary[key]) == type(dictionary): # directory
-            print('Creating a directory')
-            path_file.write('Creating a directory\n')
             os.mkdir(path + key + r'\\')
             parse(dictionary[key], path + key + r'\\')
 
 def main():
-    path_file.write('main\n')
-
     json_data = open(r'D:\\Code\\scripts\\2020\\structure-me\\settings\\settings.json', 'r')
 
     loaded = json.load(json_data)
-    path_file.write('loaded\n')
 
     parse(loaded, path)
 
@@ -42,5 +28,5 @@ def main():
 
     return 0
 
-main()
-path_file.close()
+if __name__ == "__main__":
+    main()
